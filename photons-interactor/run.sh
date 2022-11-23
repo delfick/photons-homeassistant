1a5a885470a70f60a823af759df19c01f7ddc9bd
Stephen Moore <stephen@delfick.com>
2022-11-23 22:14:40 +1100

    config: Add option to make logs silent (errors only)
    Fixes #4

--- photons-interactor/run.sh
+++ photons-interactor/run.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/with-contenv bashio
 
+SILENT="$(bashio::config 'silent')"
+if [[ $SILENT == "true" ]]; then
+    export PHOTONS_SILENT_BY_DEFAULT=1
+fi
+
 CONFIG="$(bashio::config 'config')"
 echo "$CONFIG" >config.json
 
